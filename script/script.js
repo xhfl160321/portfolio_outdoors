c704009116d07e7783d3041fcd3e661688fdca5c
sueng0422 <sueng0422@naver.com>
2023-02-14 20:56:48 +0900

    commit

---
+++ script/script.js
@@ -0,0 +1,50 @@
+gsap.registerPlugin(ScrollTrigger);
+// REVEAL //
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 80%",
+    end: "bottom 20%",
+    markers: true,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeave: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { y: -100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
+
+// 이미지 호버 시 컨텐츠
+$(".p2ImgCon p").hide();
+$(".p2Img > ul > li").hover(function(){
+  $(this).find(".p2ImgCon p").stop().slideDown(1300);
+}, function(){
+  $(this).find(".p2ImgCon p").stop().slideUp(1300);
+});
